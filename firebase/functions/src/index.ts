1c0eef1df3345cbff2a0476de042543c4ffedeb5
Luis Iturrios <luisiturrios1@gmail.com>
2024-12-28 20:15:50 -0700

    Mover proyecto de firebase a su propio repo

--- firebase/functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as admin from "firebase-admin";
-import { updateRifaOnRatingWritten } from "./updateRifaOnRatingWritten";
-import { updateRifas } from "./updateRifas";
-
-admin.initializeApp();
-
-export { updateRifaOnRatingWritten, updateRifas };
